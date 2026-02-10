891d767cc0e7172102533ba172e5bb5f8f58bfdf
Sofus Addington <sofus@addington.dk>
2026-02-10 09:45:01 +0100

    Remove the cat again

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -1,12 +1,10 @@
 #include "custom_status_screen.h"
 #include "widgets/battery_status.h"
-#include "widgets/bongo_cat.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static struct zmk_widget_dongle_battery_status dongle_battery_status_widget;
-static struct zmk_widget_bongo_cat bongo_cat_widget;
 
 lv_style_t global_style;
 
@@ -24,11 +22,9 @@ lv_obj_t *zmk_display_status_screen() {
     lv_style_set_text_line_space(&global_style, 1);
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
 
-    zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
     zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
 
-    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_RIGHT_MID, -6, 3);
-    lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_LEFT_MID, 0, 0);
+    lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_CENTER, 0, 0);
 
     return screen;
 }
